2a4d175bbaf242df09287b3a05166cd13c69ed09
walyenok <kiselewwww@gmail.com>
2021-11-18 16:47:15 +0300

    Da)

---
+++ kiselev_arhiv2.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 192.168.4.211    Database: kiselev
+-- ------------------------------------------------------
+-- Server version	5.7.22-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `arhiv2`
+--
+
+DROP TABLE IF EXISTS `arhiv2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `arhiv2` (
+  `nomer_zapisi` int(11) NOT NULL AUTO_INCREMENT,
+  `FIO` varchar(65) CHARACTER SET utf16 DEFAULT NULL,
+  `Gruppa` varchar(45) CHARACTER SET utf16 DEFAULT NULL,
+  `Stipendiya` float DEFAULT NULL,
+  PRIMARY KEY (`nomer_zapisi`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `arhiv2`
+--
+
+LOCK TABLES `arhiv2` WRITE;
+/*!40000 ALTER TABLE `arhiv2` DISABLE KEYS */;
+INSERT INTO `arhiv2` VALUES (1,'Абобус','4п3',666),(2,'tr2','4п3',NULL),(3,'d','d',32);
+/*!40000 ALTER TABLE `arhiv2` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-18 13:50:55
---
+++ kiselev_student.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 192.168.4.211    Database: kiselev
+-- ------------------------------------------------------
+-- Server version	5.7.22-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `N_stud` int(11) NOT NULL AUTO_INCREMENT,
+  `FIO` varchar(75) DEFAULT NULL,
+  `D_R` date DEFAULT NULL,
+  `Gruppa` varchar(45) DEFAULT NULL,
+  `Stipendiya` float DEFAULT NULL,
+  `Adres` varchar(45) DEFAULT NULL,
+  `Stip_new` float DEFAULT NULL,
+  `PREMiYA` float DEFAULT NULL,
+  `YYY` int(11) DEFAULT NULL,
+  `MMM` int(11) DEFAULT NULL,
+  `DDD` int(11) DEFAULT NULL,
+  `cod_spec` int(11) DEFAULT NULL,
+  `col` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`N_stud`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf16;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'Сидоров','1992-07-23','23ao1',3522,'3321',3512,1761,1992,7,23,3091,'col1'),(3,'Григорьев','1992-04-19','4p2',3682,'8811',3672,1841,1992,4,19,3091,'col1'),(4,'Данилова','1992-06-11','4p2',4802,'8811',4792,2401,1992,6,11,3091,'col2'),(6,'Серова','1991-02-25','23ao1',3602,'3322',3592,7509,1991,2,25,3091,'col2'),(8,'Смирнов','1991-07-16','4p2',3682,'8811',3672,491,1991,7,16,3091,'col1'),(9,'Подгорнова','1991-09-25','4п3',4802,'4466',4792,509,1991,9,25,3091,'col2'),(11,'Иванов','1999-02-11','4p3',46,'4433',36,509,1999,2,11,3091,'col2'),(12,'Абобус','1999-02-11','4p3',8666,'2121212',8656,491,1999,2,11,2612,'col1'),(13,'tr2','1999-02-11','23ao1',446,NULL,436,512,1999,2,11,2612,'col2'),(14,'gay','2002-01-11','23ao1',398,NULL,388,1512,2002,1,11,2612,'col2'),(15,'Боба','2021-05-05','slave',4802,NULL,4792,491,2021,5,5,111,'col1'),(16,'Чел','2010-03-17','n_word',NULL,'11',NULL,NULL,2010,3,17,1212,'col2'),(17,'гомик','2020-01-11','ыы',NULL,NULL,NULL,1509,2020,1,11,0,'col2');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-18 13:50:55
